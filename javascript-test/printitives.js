1d146109d39487aeed7f60de9d6c04cbe8acc386
Flo820 <55712442+Flo820@users.noreply.github.com>
2019-10-28 12:18:02 -0700

    JavaScript in class

---
+++ javascript-test/printitives.js
@@ -0,0 +1,24 @@
+const myString = "Leanring JavaScript";
+const myFirstNumber = 3;
+const myBolean = true;
+const myNull = null;
+let myUndefined;
+
+myUndefined = "Learning more JavaScript in class";
+
+var oldStyle = "This is the way we used to do it.";
+let newChangingStyle = "This allows you to declare a variable that can be changed";
+const consantVariable = "This variable can not be changed. Except.....";
+
+const quotable = 'He said, "hello". That\'s all folks.';
+
+console.log(myString);
+console.log('TYPE', typeof myString);
+console.log(myFirstNumber);
+console.log('TYPE', typeof myFirstNumber);
+console.log('TYPE', typeof myBolean);
+console.log(myNull);
+console.log('TYPE', typeof myNull);
+console.log(myUndefined);
+console.log('TYPE', typeof myUndefined);
+console.log('3 plus 25', myFirstNumber + 25);
