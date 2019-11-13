8acfe864e614d69b3c54a0a8f9f001ce83ac8000
Florencio Pacheco on school computer <lencho2001@outlook.com>
2019-11-13 12:19:50 -0800

    changes

--- javascript-test/function-basics.js
+++ javascript-test/function-basics.js
@@ -1,13 +1,23 @@
-function simpleFunction() {
+window.onload = () => {
+    // All code goes in here.
+    const simpleFunctionDisplay = document.getElementById('simpleFunctionDisplay');
+    const functionCallsAction = document.getElementById('functionCallsAction');
+
+    functionCallsAction.onclick = simple;
+    
+    function simpleFunction() {
     console.log("functions should consist of statements designed to perform a single task");
-}
+    simpleFunctionDisplay.textContent = "simple";
+ }
 
 function functionParameter(value) {
 console.log(value);
 }
+
 function functionReturn(addResult) {
     return "The add function returned: " + addResult;
 }
+
 function add(a,b){
     return a + b;
 }
@@ -20,3 +30,5 @@ const addResult = add(2, 3);
 
 const returnValue = functionReturn(addResult);
 console.log(returnValue);
+
+}
